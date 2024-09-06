cd1593dfbd62a94b6c8fbc863e66250d3a7cb937
Jinoo Sim <jinoosim@gmail.com>
2024-09-06 10:24:38 +0900

    removed dir path validation

--- exercies/web_server.py
+++ exercies/web_server.py
@@ -110,11 +110,11 @@ def run_server(base_dir, port=8080):
 
 
 def main():
-    if len(sys.argv) != 2:
-        print("사용법: python webserver.py <directory_path>")
-        sys.exit(1)
-
-    base_dir = os.path.abspath(os.path.expanduser(sys.argv[1]))
+    # if len(sys.argv) != 2:
+    #     print("사용법: python webserver.py <directory_path>")
+    #     sys.exit(1)
+    base_dir = "/root/html/index.html"
+    # base_dir = os.path.abspath(os.path.expanduser(sys.argv[1]))
     if not os.path.isdir(base_dir):
         print(f"오류: {base_dir}는 유효한 디렉토리가 아닙니다.")
         sys.exit(1)
