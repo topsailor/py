5256e506a63534a950ce0e6fec9be24817afc057
Jinoo Sim <jinoosim@gmail.com>
2024-09-06 10:09:35 +0900

    changed to use syslog-ng

--- exercies/log2html.py
+++ exercies/log2html.py
@@ -15,7 +15,7 @@ def parse_log_file(file_path, num_lines):
                 if match:
                     timestamp_str, log_level, message = match.groups()
                     timestamp = datetime.strptime(
-                        timestamp_str[:-5], "%Y년 %m월 %d일 %H시 %M분 %S초"
+                        timestamp_str[:-5], "%Y-%m-%dT%H:%M:%S"
                     )
                     parsed_logs.append(
                         {
